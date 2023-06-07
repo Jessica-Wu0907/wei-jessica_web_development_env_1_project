1aaa6d132bfd7e5c526204896df7ebe796cb60c1
alex20180614 <alex20180614@gmail.com>
2023-06-07 19:14:24 -0400

    add
    add

--- signup.php
+++ signup.php
@@ -3,11 +3,11 @@
 
     include('dbh.inc.php');
     if(isset($_POST['submit'])){
-    $first=$_Post["first"];
-    $last=$_Post["last"];
-    $email=$_Post["email"];
-    $uid=$_Post["uid"];
-    $pwd=$_Post["pwd"];
+    $first=$_POST["first"];
+    $last=$_POST["last"];
+    $email=$_POST["email"];
+    $uid=$_POST["uid"];
+    $pwd=$_POST["pwd"];
 
     $sql = "INSERT INTO users(user_first,user_last,user_email,user_uid,user_pwd) VALUES('$first','$last', '$email', 
     '$uid','$pwd');";
@@ -17,7 +17,7 @@
     if(mysqli_query($conn,$sql))
     {
         mysqli_close($conn);
-        header("Location:index.php?signup=success");
+        header("Location:about.php?signup=success");
     }
     else{
         echo 'query error' . mysqli_error($conn);
